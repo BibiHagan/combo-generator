0ec4537bb74c0d45289d920a88e72e4f038a59ad
Bibi Hagan <bhagan@seek.com.au>
2020-05-26 09:43:00 +1000

    refactor of code to form base project

---
+++ src/script.js
@@ -0,0 +1,28 @@
+var updater = function () {
+  var combo = getRandomNumber() + getRandomLetter();
+  var combos = document.getElementById('combination');
+  combos.innerHTML = combo;
+
+  function getRandomNumber() {
+    return Math.floor(Math.random() * 10) + 1;
+  }
+
+  function getRandomLetter() {
+    number = Math.floor(Math.random() * 4);
+
+    switch (number) {
+      case 0:
+        return 'A';
+        break;
+      case 1:
+        return 'B';
+        break;
+      case 2:
+        return 'C';
+        break;
+      case 3:
+        return 'D';
+        break;
+    }
+  }
+};
